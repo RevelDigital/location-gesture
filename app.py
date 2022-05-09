71b39a25d360b48fde0692e41cc8c0bf23f7ea5a
Christian Koski <64987609+ckosk@users.noreply.github.com>
2022-05-08 21:27:45 -0500

    Polishing and Better Comments

--- app.py
+++ app.py
@@ -19,14 +19,15 @@ def startLimit():
     Timer(.1, startLimit).start()
 startLimit();
 
-# a function that calculates distance between two points
+#Function that calculates distance between two points
 def distance(x1, y1, x2, y2):
     """
     calculates distance between two points
     x1, y1: x and y coordinates of point 1
     x2, y2: x and y coordinates of point 2
     """
-    return ((x2 - x1)**2 + (y2 - y1)**2)**0.5   #calculate distance between two points
+    #Calculate distance between two points
+    return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 
 def json_to_dict(filename):
     with open(filename) as f:
@@ -36,9 +37,9 @@ def json_to_dict(filename):
 
 try:
     setup_file = json_to_dict(r'/home/pi/Desktop/location-gesture-main/SETUP.json')
-    #opens serial port on default 9600,8,N,1 no timeout # Tutorial https://stackoverflow.com/questions/16701401/python-and-serial-how-to-send-a-message-and-receive-an-answer
+    #Opens serial port on default 9600,8,N,1 no timeout
     ser = serial.Serial("/dev/ttyS0")
-    #prints the port that is really being used
+    #Prints the port that is really being used
     print("Serial port being used: " + str(ser.name))
     if(setup_file["serialOutput"] == "True"):
         print("Serial Output: ON")
@@ -49,52 +50,50 @@ try:
     else:
         print("WILL NOT Output in Terminal")
 
-    # initialize mediapipe
+    #Initialize mediapipe
     mpHands     = mp.solutions.hands            # hands = mpHands(x1, y1, x2, y2)
     hands       = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7) #more hands
     mpDraw      = mp.solutions.drawing_utils    # draw on the image
 
-    # Load class names
+    #Load class names
     f           = open('/home/pi/Desktop/location-gesture-main/gesture.names', 'r')    #
     classNames  = f.read().split('\n')          #
     f.close()                                   #   
 
-    #which webcam to use 0 = main, 1 = secondary etc
+    #Which webcam to use 0 = main, 1 = secondary etc
     which_webcam = 0
-    # Initialize the webcam
+    #Initialize the webcam
     cap         = cv2.VideoCapture(which_webcam, cv2.WND_PROP_FULLSCREEN)
-    # average x and y coordinates of the hands
+    #Average x and y coordinates of the hands
     avx_list, avy_list, size_list  = [], [], []    #average x and y points
 
-    # average x and y of hand
+    #Average x and y of hand
     avx, avy, size        = 0, 0, 0                #             
 
-    # is a hand detected
-    activate_gesture    = False #determines whether the gesture is activated
+    #Is a hand detected?
+    activate_gesture    = False
 
     while True:
-        # Read each frame from the webcam
+        #Read each frame from the webcam
         _, frame        = cap.read()                #
         x, y, c         = frame.shape               # 
          
-        #dimensions = cap.shape
-        #width = cap.shape[1]
         width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         height  = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-        # half of frame width
+        #Half of frame width
         half_width  = int(width/2)
 
-        # first quarter of frame width
+        #First quarter of frame width
         first_quarter_width  = int(width/4)
 
-        # last quarter of frame width
+        #Last quarter of frame width
         last_quarter_width  = int(width - first_quarter_width)
 
-        # first eighth of frame width
+        #First eighth of frame width
         first_eighth_width  = int(width/8)
 
-        # last eighth of frame width
+        #Last eighth of frame width
         last_eighth_width  = int(width - first_eighth_width)
 
         if(setup_file["quadrantVisualization"] == "True"):
@@ -104,59 +103,59 @@ try:
             cv2.line(img=frame, pt1=(last_eighth_width, 40), pt2=(last_eighth_width, int(height-40)), color=(255, 0, 0), thickness=3, lineType=8, shift=0) 
             cv2.line(img=frame, pt1=(half_width, 20), pt2=(half_width, int(height-20)), color=(0, 0, 255), thickness=2, lineType=8, shift=0)
 
-        # actual frame size IDK why id does this but this is the number that maintains accuracy for the hand detection
+        #Actual frame size
         width = 480
 
-        # half of frame width
+        #Half of frame width
         half_width  = int(width/2)                  #
 
-        # first quarter of frame width
+        #First quarter of frame width
         first_quarter_width  = int(width/4)         #
 
-        # last quarter of frame width
+        #Last quarter of frame width
         last_quarter_width  = int(width - first_quarter_width)
 
-        # first eighth of frame width
+        #First eighth of frame width
         first_eighth_width  = int(width/8)          #
 
-        # last eighth of frame width
+        #Last eighth of frame width
         last_eighth_width  = int(width - first_eighth_width)
         
-        # Flip the frame vertically
+        #Flip the frame vertically
         frame           = cv2.flip(frame, 1)        #
         framergb        = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Get hand landmark prediction
+        #Get hand landmark prediction
         result          = hands.process(framergb)   #
 
-        # The predicted gesture
+        #The predicted gesture
         className       = ''                        #
         
-        # average x and y of palm
+        #Average x and y of palm
         avg_palm_x, avg_palm_y      = 0, 0          #
         
         #palm           : x, y
         palm            = [0,0]                     #x,y
-        # thumb         : tip = 4,  base = 1
+        #thumb         : tip = 4,  base = 1
         thumbRaised     = True                      #is the thumb raised?
         thumbFingers_x, thumbFingers_y  = [0, 0], [0, 0]    #tip, base
-        # index         : tip = 8,  base = 5
+        #index         : tip = 8,  base = 5
         indexRaised     = True                      #is the index finger raised?
         indexFingers_x, indexFingers_y  = [0, 0], [0, 0]    #tip, base
-        # middle        : tip = 12, base = 9
+        #middle        : tip = 12, base = 9
         middleRaised    = True                      #is the middle finger raised?
         middleFingers_x, middleFingers_y = [0, 0], [0, 0]   #tip, base
-        # ring          : tip = 16, base = 13
+        #ring          : tip = 16, base = 13
         ringRaised      = True                      #is the ring finger raised?
         ringFingers_x, ringFingers_y   = [0, 0], [0, 0]     #tip, base
-        # pinky         : tip = 20, base = 17
+        #pinky         : tip = 20, base = 17
         pinkyRaised     = True                      #is the pinky finger raised?
         pinkyFingers_x, pinkyFingers_y  = [0, 0], [0, 0]    #tip, base
         
-        # post process the result
+        #Post process the result
         if  result.multi_hand_landmarks:
-            landmarks   = []     #landmarks of each hand
-            # draw lines between last five average x and y cooordinates except the first and last
+            landmarks   = []     #Landmarks of each hand
+            #Draw lines between last five average x and y cooordinates except the first and last
             
             for handslms in result.multi_hand_landmarks:
                 avx, avy= 0, 0      #average x, y of all the hand
@@ -164,10 +163,10 @@ try:
 
                 for lm in handslms.landmark:
 
-                    lmx = round(int(lm.x * x), 3) #x of landmark
-                    lmy = round(int(lm.y * y), 3) #y of landmark
-                    avx += lmx #add x of landmark to the average x
-                    avy += lmy #add y of landmark to the average y
+                    lmx = round(int(lm.x * x), 3) #x coord of landmark
+                    lmy = round(int(lm.y * y), 3) #y coord of landmark
+                    avx += lmx
+                    avy += lmy
                     
                     #label each point with a number
                     cv2.putText(frame, str(index), (int(lmx*1.3), int(lmy*0.8)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (12*index, 0, 255-12*index), 1)
@@ -226,21 +225,21 @@ try:
                     
                     index += 1
                 
-                #get range of x and y, for use in calculating the hand's size
+                #Get range of x and y, for use in calculating the hand's size
                 x_range = max(landmarks, key=lambda x: x[0])[0] - min(landmarks, key=lambda x: x[0])[0]
                 y_range = max(landmarks, key=lambda x: x[1])[1] - min(landmarks, key=lambda x: x[1])[1]
 
-                #size of hand relative to screen
+                #Size of hand relative to screen
                 size = round((x_range+y_range)/(width+height), 2)
 
-                # average of all the hand
+                #Average of all the hand
                 avx, avy                    = round(avx / len(handslms.landmark), 1), round(avy / len(handslms.landmark), 1)
-                #average of the hand in relation to the palm
+                #Average of the hand in relation to the palm
                 avg_palm_x, avg_palm_y      = round((palm[0] + avx)/2, 1), round((palm[1] + avy)/2, 1)
-                #distance of the thumb in relation to the palm
+                #Distance of the thumb in relation to the palm
                 dist_hand                   = distance(avg_palm_x, avg_palm_y, avx, avy)
                 
-                # store last fifteen average x and y cooordinates
+                #Store last fifteen average x and y cooordinates
                 if  avx != 0 and avy != 0:
                     avx_list.append(avx)
                     avy_list.append(avy)
@@ -250,10 +249,10 @@ try:
                     avy_list.pop(0)
                     size_list.pop(0)
 
-                # setting a sensitivity for how much each finger is considered raised or not
+                #Setting a sensitivity for how much each finger is considered raised or not
                 sensitivity = 0.6
                 
-                #index finger exists in the correct range to determine if the hand is in frame
+                #Index finger exists in the correct range to determine if the hand is in frame
                 if  indexFingers_x[0]       != 0 :
                     thumbRaised             = (dist_hand < distance(thumbFingers_x[0],  thumbFingers_y[0],  avg_palm_x, avg_palm_y)*sensitivity)
                     indexRaised             = (dist_hand < distance(indexFingers_x[0],  indexFingers_y[0],  avg_palm_x, avg_palm_y)*sensitivity)
@@ -261,7 +260,7 @@ try:
                     ringRaised              = (dist_hand < distance(ringFingers_x[0],   ringFingers_y[0],   avg_palm_x, avg_palm_y)*sensitivity)
                     pinkyRaised             = (dist_hand < distance(pinkyFingers_x[0],  pinkyFingers_y[0],  avg_palm_x, avg_palm_y)*sensitivity)
                     
-                #print if fingers are raised or not
+                #Print if fingers are raised or not
                 cv2.putText(frame, 'Thumb Raised: ' + str(thumbRaised),  (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1)
                 if thumbRaised == True:
                     thumbR = 1
@@ -296,13 +295,13 @@ try:
                 if(setup_file["crashChrisComputer"] == "True"):
                     #reset index
                     index = 0
-                    # Drawing landmarks on frames
+                    #Drawing landmarks on frames
                     mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
 
-                #reset index
+                #Reset index
                 index = 0
 
-                # Drawing landmarks on frames
+                #Drawing landmarks on frames
                 mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
         
         # if the hand is in the frame
@@ -312,10 +311,10 @@ try:
                 #move mouse to the center of the palm
                 mouse.move(int((avg_palm_x-200)*8-400), int((avg_palm_y-200)*4-400))
             
-            #is the hand in a held position?
+            #Is the hand in a held position?
             isHeld = (not indexRaised) and (not middleRaised) and (not ringRaised) and (not pinkyRaised)
 
-            #check which quadrent the avg palm is
+            #Check which quadrant the avg palm is in
             if(avg_palm_x>0 and avg_palm_x<first_eighth_width):
                 quadrent = 1
             elif(avg_palm_x>first_eighth_width  and avg_palm_x<first_quarter_width):
@@ -329,19 +328,19 @@ try:
             else:
                 quadrent = 0
 
-            #show the quadrent the hand is in
+            #Display which quadrant the hand is in
             cv2.putText(frame, str(str(quadrent) + ", " + str(avx) + ", " + str(avy) + ", " + str(isFist)),  (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
             if(setup_file["serialOutput"] == "True" and canSend):
                 if  indexFingers_x[0]       != 0 :
                     ser.write(bytes('gesture' + '|' + str(quadrent) + '|' + str(round(avx)) + '|' + str(round(avy)) + '|' + str(round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))) + '|' + str(round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))+ '|' + str(indexR) + '|' + str(middleR) + '|' + str(ringR) + '|' + str(pinkyR) + '|' + str(isFist) + '\n', encoding='utf8'))
                 canSend = False
-            #show hand size
+            #Display hand size
             cv2.putText(frame, 'size: ' + str(size),  (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
             
             if(setup_file["outputInTerminal"] == "True"):
                 if  indexFingers_x[0]       != 0 :
                     print(str(str(quadrent) + ', ' + str(round(avx)) + ', ' + str(round(avy)) + ', ' + str(round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))) + ', ' + str(round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))) + ', ' + str(indexR) + ', ' + str(middleR) + ', ' + str(ringR) + ', ' + str(pinkyR) + ', ' + str(isFist)))
-                #(quadrent, x-coord of hand, y-coord of hand, camera res width, camera res height, index finger raised, middle finger raised, ring finger raised, pinky finger raised, is fist?)
+                #(quadrant, x-coord of hand, y-coord of hand, camera res width, camera res height, index finger raised, middle finger raised, ring finger raised, pinky finger raised, is fist?)
 
             #if the hand is not held
             if(setup_file["mouseControl"] == "True"):
@@ -358,28 +357,28 @@ try:
                         print("right clicked")
                         time.sleep(0.3)
             
-                #if the hand is held
+                #If the hand is held
                 if  isHeld:
                     if  not mouse.is_pressed('left'):
                         mouse.press('left')
                         print("pressed")
             
-        # Show the final output
+        #Show the final output
         cv2.namedWindow("Output", cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty("Output", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         if(setup_file["videoFeedPortrait"] == "True"):
             frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
         cv2.imshow("Output", frame)
 
-        # Press q to quit
+        #Press q to quit
         if  cv2.waitKey(1) == ord('q'):
             ser.close()
             break
 
-    # release the webcam and destroy all active windows
+    #Release the webcam and destroy all active windows
     cap.release()
 
-    # destroy all windows
+    #Destroy all windows
     cv2.destroyAllWindows()
 except serial.SerialException as e:
     print(str(e))
